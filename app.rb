a772e0eab21b53c455d82c4108c7e43d48faefb7
helenswanson <hhyswanson@gmail.com>
2014-06-10 15:59:46 -0400

    Show page complete.

--- app.rb
+++ app.rb
@@ -5,12 +5,15 @@ require "sinatra/activerecord"
 require_relative 'models/contact'
 
 get '/' do
+  redirect '/contacts'
+end
+
+get '/contacts' do
   @contacts = Contact.all
   erb :index
 end
 
 get '/contacts/:id' do
-  @id = params[:id]
-
+  @contact = Contact.find(params[:id])
   erb :show
 end
--- models/contact.rb
+++ models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   def name
     [first_name, last_name].join(' ')
   end
+
 end
