4300a7085db7432eab839c685ef8673a0dbe8f19
helenswanson <hhyswanson@gmail.com>
2014-06-10 14:59:51 -0400

    Initial commit. All tests working. No show page

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+require_relative 'models/contact'
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+  @id = params[:id]
+
+  erb :show
+end
